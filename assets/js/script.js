500d0e86a4271f67e4a3400e17561ecb847fd78a
Ilham D. Sofyan <ilhamdsofyan@gmail.com>
2024-01-10 17:53:55 +0700

    Activate search button feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
-// Toggle Navbar Class Active
+// On hamburger menu clicked
 const navbarNav = document.querySelector(".navbar-nav");
-
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 
-// On hamburger menu clicked
-hamburgerMenu.onclick = () => {
+hamburgerMenu.onclick = (e) => {
+  e.preventDefault();
+
   navbarNav.classList.toggle("active");
+};
+
+// Toggle search form
+const navbarSearch = document.querySelector("#search-button");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+navbarSearch.onclick = (e) => {
+  e.preventDefault();
+
+  searchForm.classList.toggle("active");
+  searchBox.focus();
 };
 
 // Outside menu click disable menu
 document.addEventListener("click", (e) => {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!navbarSearch.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
